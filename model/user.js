4e5cd8afaa4e637b8b8528797ac0bf50955bc05c
Anvith10 <anvith1919@gmail.com>
2022-01-10 22:06:10 +0530

    customer schema without foreign key constraint

---
+++ model/user.js
@@ -0,0 +1,21 @@
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../db/connect");
+
+
+const customers = sequelize.define("series", {
+  email_id: {
+    type: DataTypes.STRING,
+        allowNull:false,
+        primaryKey: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  customer_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  }
+});
+
+module.exports = customers;
