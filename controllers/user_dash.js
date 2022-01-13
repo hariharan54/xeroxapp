42dd4474585846dcfe6d14454ce8347572108f5e
Anvith10 <anvith1919@gmail.com>
2022-01-13 20:28:27 +0530

    Add user dashboard js file

---
+++ controllers/user_dash.js
@@ -0,0 +1,33 @@
+const printouts = require("../model/printout")
+const customers = require("../model/user")
+
+
+exports.userOrders = async (req,res,userData) =>{
+    try{
+    const orders = await printouts.findAll({ where: {
+        customer_user_id:userData.email_id
+      }});
+    if(!orders){
+        res.status(402).json({ errors: "No order yet" });
+    }
+    return res.send(orders)
+}catch(err){
+    res.status(402).json({ errors: err });
+}
+}
+
+exports.newOrder = async (req,res) =>{
+    const orderData = req.body;
+    if(!orderData){ res.status(402).json({ errors: "No order details entered" });}
+    const printOrder = new customers(orderData);
+    printOrder= await printOrder.save()
+    return res.send(printOrder);
+}
+
+exports.payment = async (req,res) =>{
+    const paymentData = req.body;
+    if(!paymentData){ res.status(402).json({ errors: "No order details entered" });}
+    const paymentDetails = new customers(paymentData);
+    paymentDetails= await paymentDetails.save()
+    return res.send(paymentDetails);
+}
